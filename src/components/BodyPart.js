0f8a7bf59a3dac4ede676c979223ae09de28b357
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-08-30 22:33:10 +0530

    border added

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import {Stack , Typography} from '@mui/material'
+import Icon from '../assets/icons/gym.png'
 
-const BodyPart = () => {
+const BodyPart = ({item , setBodyPart , bodyPart}) => {
   return (
-    <div>BodyPart</div>
+    <Stack
+    type = "button"
+    alignItems="center"
+    justifyContent = "center"
+    className  = "bodyPart-card"
+    sx = {
+        
+       bodyPart === item ?   {
+        borderTop : '4px solid #ff2625',
+        backgroundColor : '#fff'
+        border
+
+       } : ' '
+
+    }
+    
+    
+    >
+
+     <img src = {Icon} alt = 'dumbbell' style={{
+       width : '40px',
+       height : '40px'
+
+     }}
+     />
+
+    </Stack>
   )
 }
 
